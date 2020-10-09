1d89510873a922291e4583f15a69d027578c7610
Jennalyn Kabiling <heyimjennaa@hotmail.com>
2020-10-08 20:56:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-// Creating constants for inputs
-const retail_price_input = document.querySelector('#retail_price')
-const county_tax_input = document.querySelector('#county_tax')
-const dispensary_tax_input = document.querySelector('#dispensary_tax')
-const total_items_input = document.querySelector('#total_items')
+// Creation of variables
+const display = document.querySelector('#display')
+const totalDisplay = document.querySelector('#total')
+const costInput = document.querySelector('#cost')
+const dispensaryInput = document.querySelector('#dispensary')
+const countyInput = document.querySelector('#county')
+const unitsInput = document.querySelector('#units')
 
-// Creating constants for displays
-const show_total_cost_display = document.querySelector('#total_cost')
+// Event Listeners
+costInput.addEventListener('input', calculateCost)
+dispensaryInput.addEventListener('input', calculateCost)
+countyInput.addEventListener('input', calculateCost)
+unitsInput.addEventListener('#units', calculateCost)
 
-// Adding event listeners for function
-retail_price_input.addEventListener('input', calculatePrice)
-county_tax_input.addEventListener('input', calculatePrice)
-dispensary_tax_input.addEventListener('input', calculatePrice)
-total_items_input.addEventListener('input', calculatePrice)
-
-// Function to calculate total price
-function calculatePrice() {
-    const retail_price_value = parseFloat(retail_price_input.value)
-    const county_tax_value = parseFloat(county_tax_input.value)
-    const dispensary_tax_value = parseFloat(dispensary_tax_input.value)
-    const show_total_cost = retail_price_value * county_tax_value * dispensary_tax_value
+// Creation of calculateCost function
+function calculateCost() {
+    const totalDisplay = parseFloat(costInput.value) 
+    const dispensaryValue = parseFloat(dispensaryInput.value) 
+    const countyValue = parseFloat(countyInput.value)
+    const unitsValue = parseFloat(unitsInput.value)
+    const totalCost = parseFloat((totalDisplay + dispensaryValue + countyValue) * (unitsValue))
+    display.innerHTML = totalCost.toFixed(2) 
+    totalDisplay.innerHTML = totalCost.toFixed(2)
 }
+
+calculateCost()
